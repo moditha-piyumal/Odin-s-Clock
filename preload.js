649509a84305b507655081b31987a6e6994ce0d7
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2026-01-26 13:33:57 +0530

    Expansion-2

--- preload.js
+++ preload.js
@@ -2,6 +2,8 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("windowControls", {
 	minimize: () => ipcRenderer.send("window:minimize"),
-	maximize: () => ipcRenderer.send("window:maximize"),
 	close: () => ipcRenderer.send("window:close"),
+
+	expand: () => ipcRenderer.send("window:expand"),
+	collapse: () => ipcRenderer.send("window:collapse"),
 });
